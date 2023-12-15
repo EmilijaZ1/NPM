81608af599801b6ceb2b02fb41b5d59145db98f5
Emilija <43810084+EmilijaZ1@users.noreply.github.com>
2023-12-15 13:41:13 +1000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+//var generateName = require('sillyname');
+
+import generateName from "sillyname";
+
+
+var sillyName = generateName();
+
+console.log(`my name is ${sillyName}`);
+
+
+import superheroes from "superheroes";
+ 
+superheroes.all;
+//=> ['3-D Man', 'A-Bomb', …]
+ 
+
+
+var name = superheroes.random();
+
+console.log(`my name is ${name}!!`);
